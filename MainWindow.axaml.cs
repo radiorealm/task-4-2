40eca8422578f17b46e8aa57d0f9116143b4b57c
Dasha <jinga08@yandex.ru>
2024-04-22 11:58:28 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace task_4_2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
